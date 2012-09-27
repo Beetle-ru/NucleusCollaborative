d8428cfccc9717effb4e82a57f78d33ac9ad3511
Beetle <abhaja.ru@gmail.com>
2012-09-27 11:35:09 +0400

    SublanceGenerator доведен до рабочего состояния, но не тестирован

---
+++ Fusion/Algorithms/SublanceGenerator/Iterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Implements;
+
+namespace SublanceGenerator
+{
+    
+    static class Iterator
+    {
+        public static RollingAverage Oxigen;
+        public static RollingAverage CarbonMonoxide;
+        public static double HotMetallMass;
+        public static Int64 HeatNumber;
+        private const int SmoothInterval = 15;
+        private static double m_oxygenStartValue ;
+        public static void Init()
+        {
+            Oxigen = new RollingAverage();
+            CarbonMonoxide = new RollingAverage();
+            HotMetallMass = 300;
+            HeatNumber = 0;
+            m_oxygenStartValue = 0;
+        }
+        public static void Renit()
+        {
+            Init();
+        }
+        public static void Iterate()
+        {
+            var oxy = Oxigen.Average(SmoothInterval);
+            var co = CarbonMonoxide.Average(SmoothInterval);
+            if (Verificate(oxy, co, HotMetallMass))
+            {
+                var fex = new ConnectionProvider.FlexHelper("Model.SublanceStart");
+                fex.AddArg("OxygenStartValue", m_oxygenStartValue);
+                fex.AddArg("CurrentOxygen", oxy);
+                fex.AddArg("CurrentCo", co);
+                fex.AddArg("CurrentHotMetallMass", HotMetallMass);
+                fex.Fire(Program.MainGate);
+            }
+        }
+        private static bool Verificate(double oxigen, double carbonMonoxide, double hotMetallMass)
+        {
+            const int transformValue = 64;
+            const int coTreshold = 15;
+            m_oxygenStartValue = OxygenStartValue(hotMetallMass, transformValue);
+            return (m_oxygenStartValue > oxigen) && (carbonMonoxide < coTreshold);
+        }
+        private static double OxygenStartValue(double hotMetallMass, int transformValue)
+        {
+            return hotMetallMass*transformValue;
+        }
+    }
+}
--- Fusion/Algorithms/SublanceGenerator/Program.cs
+++ Fusion/Algorithms/SublanceGenerator/Program.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ConnectionProvider;
+using Converter;
 
 namespace SublanceGenerator
 {
     class Program
     {
+        public static Client MainGate;
         static void Main(string[] args)
         {
+            var o = new HeatChangeEvent();
+            MainGate = new Client(new Listener());
+            MainGate.Subscribe();
+            Iterator.Init();
+            Console.WriteLine("Press Enter for exit");
+            Console.ReadLine();
         }
     }
 }
