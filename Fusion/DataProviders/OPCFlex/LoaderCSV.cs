3a492d1d0815e932a074fffc5bd066d5023c5787
Beetle <abhaja.ru@gmail.com>
2012-10-25 09:42:45 +0400

    В OPCFlex добавлен файл лоадера

---
+++ Fusion/DataProviders/OPCFlex/LoaderCSV.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Converter;
+
+namespace OPCFlex
+{
+    class LoaderCSV
+    {
+        public List<FlexEvent> DescriptionFlexList;
+        public string Path;
+        public LoaderCSV()
+        {
+            DescriptionFlexList = new List<FlexEvent>();
+        }
+
+        public void Load (string path)
+        {
+            
+        }
+    }
+}
