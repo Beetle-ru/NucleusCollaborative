ff8a46f002ac0508f971367116b83d833f588601
Beetle <abhaja.ru@gmail.com>
2012-10-29 09:54:55 +0400

    В проект коррекций добавлены необходимые файлы

---
+++ Fusion/Algorithms/CorrectionCT/Listener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ConnectionProvider;
+using Core;
+using Converter;
+using CommonTypes;
+using ConnectionProvider.MainGate;
+using Implements;
+//using System.ServiceModel;
+//using System.Windows.Forms;
+
+namespace CorrectionCT
+{
+    class Listener : IEventListener
+    {
+        public Listener()
+        {
+            InstantLogger.log("Listener", "Started", InstantLogger.TypeMessage.important);
+        }
+        
+
+        public void OnEvent(BaseEvent newEvent)
+        {
+            using (var l = new Logger("Listener"))
+            {
+       
+            }
+        }
+    }
+}
