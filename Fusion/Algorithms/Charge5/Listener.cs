f2281dfbfac02108443084c10873fcfffbb3f698
Beetle <abhaja.ru@gmail.com>
2012-11-02 14:58:44 +0400

    Добавлен подпроект Шихта5

---
+++ Fusion/Algorithms/Charge5/Listener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ConnectionProvider;
+using Core;
+using Converter;
+using CommonTypes;
+using ConnectionProvider.MainGate;
+using Implements;
+//using System.ServiceModel;
+//using System.Windows.Forms;
+
+namespace Charge5
+{
+    class Listener : IEventListener
+    {
+        public Listener()
+        {
+            InstantLogger.log("Listener", "Started", InstantLogger.TypeMessage.important);
+        }
+        
+
+        public void OnEvent(BaseEvent newEvent)
+        {
+  
+        }
+    }
+}
---
+++ Fusion/Algorithms/Charge5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using ConnectionProvider;
+using Converter;
+
+namespace Charge5
+{
+    class Program
+    {
+        public static Client MainGate;
+        public static Configuration MainConf;
+        public static char Separator;
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Charge5 is running, press enter to exit");
+            Console.ReadLine();
+        }
+        static void Init()
+        {
+            MainConf = System.Configuration.ConfigurationManager.OpenExeConfiguration("");
+            var o = new HeatChangeEvent();
+            MainGate = new Client(new Listener());
+            MainGate.Subscribe();
+
+            Separator = MainConf.AppSettings.Settings["separator"].Value.ToArray()[0];
+            //MatrixT.FileName = MainConf.AppSettings.Settings["matrixT"].Value;
+        }
+    }
+}
