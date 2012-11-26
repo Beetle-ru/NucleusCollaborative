451ae75b91dc19540ed2e6b7d05f4664710007ee
Beetle <abhaja.ru@gmail.com>
2012-11-26 05:14:48 +0400

    Добавленны входные и выходные данные, старые файлы раскиданы по папкам

---
+++ Fusion/Algorithms/Charge5/Data/InData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charge5
+{
+    class InData
+    {
+        /// <summary>
+        /// Тип стали 0 -- 7
+        /// </summary>
+        public int SteelType;
+
+        /// <summary>
+        /// Кремний в чугуне
+        /// </summary>
+        public double SiHi;
+
+        /// <summary>
+        /// Температура чугуна
+        /// </summary>
+        public int THi;
+
+        /// <summary>
+        /// Масса чугуна, если 0 то считать выключенным
+        /// </summary>
+        public int MHi;
+
+        /// <summary>
+        /// Масса лома, если 0 то считать выключенным
+        /// </summary>
+        public int MSc;
+
+        /// <summary>
+        /// Последующая обработка на УВС
+        /// </summary>
+        public bool IsProcessingUVS;
+    }
+}
---
+++ Fusion/Algorithms/Charge5/Data/OutData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charge5
+{
+    class OutData
+    {
+        /// <summary>
+        /// Масса чугуна
+        /// </summary>
+        public int MHi;
+
+        /// <summary>
+        /// Масса скрапа
+        /// </summary>
+        public int MSc;
+
+        /// <summary>
+        /// Масса извести
+        /// </summary>
+        public int MLi;
+
+        /// <summary>
+        /// Масса доломита или МАХГ
+        /// </summary>
+        public int MDlm;
+
+        /// <summary>
+        /// Масса фома
+        /// </summary>
+        public int MFom;
+
+        /// <summary>
+        /// Масса долмс
+        /// </summary>
+        public int MDlms;
+    }
+}
