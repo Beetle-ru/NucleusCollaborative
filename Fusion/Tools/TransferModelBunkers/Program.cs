8f2f3e1beae256787e14a16c96ce58052d09bb46
Dimitry Obuvalin <obuvalin@gmail.com>
2012-12-28 12:39:18 +0400

    simulate substance names + opc direct read

---
+++ Fusion/Tools/TransferModelBunkers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Converter;
+using Implements;
+
+namespace TransferModelBunkers
+{
+    class Program
+    {
+        private static ConnectionProvider.Client m_listenGate;
+        public static ConnectionProvider.Client m_pushGate;
+        static void Main(string[] args)
+        {
+            var o = new HeatChangeEvent();
+            var mainConf = System.Configuration.ConfigurationManager.OpenExeConfiguration("");
+            m_listenGate = new ConnectionProvider.Client(new Listener());
+            m_listenGate.Subscribe();
+            m_pushGate = new ConnectionProvider.Client();
+            Thread.Sleep(1000);
+            // запрашиваем привязку бункеров к материалам
+            m_pushGate.PushEvent(new OPCDirectReadEvent() { EventName = typeof(BoundNameMaterialsEvent).Name });
+            // навески
+            m_pushGate.PushEvent(new OPCDirectReadEvent() { EventName = typeof(visAdditionTotalEvent).Name });
+            Thread.Sleep(1000);
+            // текущий номер плавки
+            m_pushGate.PushEvent(new OPCDirectReadEvent() { EventName = typeof(HeatChangeEvent).Name });
+            Console.WriteLine("TransferModelBunkers is running, press enter to exit");
+            Console.ReadLine();
+            Console.WriteLine("Bye!");
+        }
+    }
+}
