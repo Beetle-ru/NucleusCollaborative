6bf3095dd8ba00f0e556b408715ef0252ec89012
Beetle <abhaja.ru@gmail.com>
2013-02-21 02:06:12 +0400

    Добавлены файлы проекта EventsRedirector

---
+++ Fusion/Tools/EventsRedirector/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConnectionProvider;
+using Converter;
+
+namespace EventsRedirector
+{
+    class Program
+    {
+        public static Client MainGateReceiver;
+        public static Client MainGateProvider;
+        static void Main(string[] args)
+        {
+            var o = new HeatChangeEvent();
+            MainGateReceiver = new Client("Receiv", new Listener());
+            MainGateReceiver.Subscribe();
+
+            MainGateProvider = new Client("Fire");
+            Console.WriteLine("Press Enter for exit");
+            Console.ReadLine();
+        }
+    }
+}
