56b807000b6c224945ce92c8fb1ed8af85ee0a75
Beetle <abhaja.ru@gmail.com>
2013-03-06 12:11:32 +0400

    Добавлен проект l1 процессор

---
+++ Fusion/Tools/LOneProcessor/Listener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using ConnectionProvider;
+using Core;
+using Converter;
+using CommonTypes;
+using ConnectionProvider.MainGate;
+using Implements;
+
+namespace LOneProcessor
+{
+    class Listener : IEventListener
+    {
+
+        public Listener()
+        {
+            InstantLogger.log("Listener", "Started\n", InstantLogger.TypeMessage.important);
+        }
+
+        public void OnEvent(BaseEvent evt)
+        {
+            using (var l = new Logger("Listener"))
+            {
+
+            }
+        }
+    }
+}
