0de8033e2e0af4fd19e51ca90a2101562625eb1c
dymon <obuvalin@gmail.com>
2013-04-03 11:50:05 +0400

    Constants for DBFlex added

---
+++ Fusion/Tools/Implements/DBFlex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Implements
+{
+    class DBFlex
+    {
+        public const string ArgEventName = "@EventName";
+        public const string ArgCommandName = "@Command";
+        public const string ArgCountName = "@Count";
+        public const string ArgErrorCodeName = "@ErrorCode";
+        public const string ArgErrorStringName = "@ErrorString";
+    }
+}
